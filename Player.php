26b337c50a1d8acc4b58853b1a2417b39f5a9c19
TommyFitzy@gmail.com <TommyFitzy@gmail.com@733785ba-0920-48d9-d2ac-302da8c93fff>
2013-09-24 18:07:31 +0000

    Back to normal

--- Player.php
+++ Player.php
@@ -1,8 +1,6 @@
 <?php
 class Player
 {
-	// Testing SVN
-
     // Variable Declarations
 	// Note: for defense name = team name
     private $name, $position, $team, $nextWeek, $fantasyTeam, $injury, $passingYards, 
