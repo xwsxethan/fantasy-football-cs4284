1bd05adb99d48f4f18e3329806ddda7305d50e6c
TommyFitzy@gmail.com <TommyFitzy@gmail.com@733785ba-0920-48d9-d2ac-302da8c93fff>
2013-10-07 04:46:55 +0000

    Added the functionality to find playerIDs as well as the player's names. It also stuffs these values into a 3D array called $league.
    $league is an array full of $teams
    $teams is an array full of $players
    $players is an associative array with keys 'ID' and 'Name'

--- GetTeams.php
+++ GetTeams.php
@@ -5,9 +5,19 @@
 $leagueID = 1164514;
 $numTeams = 10;
 
+// 3D array to store all the teams in
+// is made up of team arrays
+$league = array();
+// Array to store all the players in 
+$team = array();
+// Associative Array to store the Player info
+$player = array();
+
+
+//$id = 8; //For testing w/o loop
 for ($id=1; $id <= $numTeams; $id++)
 	{
-	print "Team ID: " . $id;
+//	print "Team ID: " . $id;
 	//Get the html for the webpage with the league ID and the current team ID
 	$url = "http://games.espn.go.com/ffl/clubhouse?leagueId=" . $leagueID . "&teamId=" . $id . "&seasonId=2013";
 	$content = file_get_contents( $url );
@@ -16,23 +26,51 @@ for ($id=1; $id <= $numTeams; $id++)
 	$content = stristr( $content, "<title>" );
 	$nameLen = stripos( $content, "-" ) - 8;
 	$teamName = substr( $content, 7, $nameLen );
-	print " (" . $teamName . ")\n";
-
+//	print " (" . $teamName . ")\n";
 
 	//Strip everything but the line with the player data
 	$content = stristr( $content, "<br clear=" );
 	$content = stristr( $content, "<script type=", true );
+	$content = stristr( $content, "\"slot_" );
 
-	$contentArr = explode("cache=", $content);
+	// Split the string into an array of strings respresenting each line of the table 
+	$contentArr = explode("<td id=", $content);
 
-	for ($i=1; $i<sizeof($contentArr); $i++)
+	// Loop through each string in the array
+	for ($i=0; $i<sizeof($contentArr); $i++)
 		{
-		$contentArr[$i] = substr( $contentArr[$i], 7);
-		if (preg_match('/([A-Z])/',$contentArr[$i][0]))
-			{
-			print stristr($contentArr[$i], "<", true);
-			print "\n\n";
-			}
+		// PlayerID is the first number. Use Regex to find it.
+		$matches = array();
+		preg_match('/([0-9]+)/', $contentArr[$i], $matches);
+		$playerID = $matches[0];
+		
+		$team[$i]['ID'] = $playerID;
+
+		// The Player's name comes after the word cache="true"
+		preg_match('/cache="true">([0-9A-Za-z \/]+)/', $contentArr[$i], $matches);
+		$playerName = substr($matches[0], 13);
+
+		$team[$i]['Name'] = $playerName;
+
+		//print $playerName . "(" . $playerID . ")" . "\n";
 		}
+	$league[$id] = $team;
 	}
+
+for ($i=1; $i<=sizeof($league); $i++)
+	{
+	for ($j=0; $j<sizeof($league[$i]); $j++)		
+		{
+		print $league[$i][$j]['Name'];
+		print "(ID: ";
+		print $league[$i][$j]['ID'];
+		print "), ";
+		}
+	print "\n\n";
+	}
+
+
+
+
+
 ?>
